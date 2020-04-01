69a0a26d9efc73dfc8456f6aff250d521a1cf0ae
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-01 16:11:51 +0100

    Display user hitpoints

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -13,4 +13,12 @@ feature 'display the player names on a separate page' do
   scenario 'display the second name' do
     expect(page).to have_content('Ed')
   end
+
+  scenario 'display the player 1 hit points' do
+    expect(page).to have_content('Phil hit points - 100')
+  end
+
+  scenario 'display the player 2 hit points' do
+    expect(page).to have_content('Ed hit points - 100')
+  end
 end

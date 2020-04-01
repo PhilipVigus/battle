a23a10216b309b1feb6b9cf582d3ebd2e5c3d88d
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-01 14:22:03 +0100

    Create form on index page

--- spec/features/tests_working_spec.rb
+++ spec/features/tests_working_spec.rb
@@ -1,6 +1,17 @@
-feature 'testing infrastructure' do
-  scenario 'correctly loads homepage with the expected content' do
+feature 'players enter their names and see them on screen' do
+  before do
     visit '/'
-    expect(page).to have_content('Testing infrastructure working!')
+  end
+
+  scenario 'player 1 can enter their name in an input field' do
+    expect(page).to have_field('player-one-name')
+  end
+
+  scenario 'player 2 can enter their name in an input field' do
+    expect(page).to have_field('player-two-name')
+  end
+
+  scenario 'page has a submit button' do
+    expect(page).to have_button('submit')
   end
 end

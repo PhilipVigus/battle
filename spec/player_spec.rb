56883976f0e43f12eba2ee73d29d815749ceb8d1
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-02 14:03:23 +0100

    Test commit

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,11 +3,11 @@ require './lib/player'
 describe Player do
   subject (:player) { Player.new('Phil') }
 
-  it "should return it's name" do
+  it "returns it's name" do
     expect(player.name).to eq('Phil')
   end
 
-  it "should have the default hit points when created" do
+  it "has default hit points when created" do
     expect(player.hit_points).to eq(Player::DEFAULT_HIT_POINTS)
   end
 

c02cace2f28a1414a0923b665a28994df7285b4e
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-02 13:03:19 +0100

    Use player class in app.rb
    This allows us to remove the use of session variables, and keep
    everything nice and clean, DRY and oop'd to the max.

--- app.rb
+++ app.rb
@@ -10,14 +10,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_one_name] = params["player-one-name"]
-    session[:player_two_name] = params["player-two-name"]
+    $player1 = Player.new(params["player-one-name"])
+    $player2 = Player.new(params["player-two-name"])
     redirect '/play'
   end
 
   get '/play' do
-    @player_one_name = session[:player_one_name]
-    @player_two_name = session[:player_two_name]
+    @player_one_name = $player1.name
+    @player_two_name = $player2.name
     erb(:play)
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,6 @@ require './lib/player'
 describe Player do
   subject (:player) {Player.new('Phil')}
   it " should return it's name" do
-    expect(subject).to respond_to(:name)
+    expect(player.name).to eq('Phil')
   end
 end

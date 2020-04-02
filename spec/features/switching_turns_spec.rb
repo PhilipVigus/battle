79360f3ac562a72d0423e7e796ebe63fc2bc0f94
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-02 15:21:28 +0100

    Update attacked screen to show who is being attacked

--- spec/features/switching_turns_spec.rb
+++ spec/features/switching_turns_spec.rb
@@ -10,4 +10,12 @@ feature 'the game should switch turns after every attack' do
     click_button('Return')
     expect(page).to have_content("Ed's turn")
   end
+
+  scenario 'the attacked page shows the correct message if it is the second player turn' do
+    sign_in_and_play
+    click_button('Attack player 2')
+    click_button('Return')
+    click_button('Attack player 2')
+    expect(page).to have_content('You have attacked Phil')
+  end
 end

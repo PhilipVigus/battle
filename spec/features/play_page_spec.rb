51669a43c345efcc98eea6359d1b80bd3c739f6a
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-02 15:34:37 +0100

    Fix attacks not reducing the correct player hit points

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -18,11 +18,4 @@ feature 'display the player status' do
     sign_in_and_play    
     expect(page).to have_content('Ed hit points - 100')
   end
-
-  scenario 'display player 2 reduced hit points after an attack' do
-    sign_in_and_play
-    click_button('Attack player 2')
-    click_button('Return')
-    expect(page).to have_content('Ed hit points - 90')
-  end
 end
